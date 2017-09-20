8e89d8e420034812618f122dec5187801e26637e
Close Screen <close.screen@gmail.com>
2017-09-20 20:51:21 +0300

    Inbank generated

---
+++ models/Inbank.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "inbank".
+ *
+ * @property integer $id
+ * @property integer $active
+ * @property integer $insalon_id
+ * @property integer $bank_id
+ * @property string $changed
+ * @property integer $changed_by_user_id
+ * @property string $state_id
+ * @property string $state_desc
+ * @property string $b1
+ * @property string $b2
+ * @property string $b3
+ * @property string $b4
+ * @property string $b5
+ */
+class Inbank extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'inbank';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['active', 'insalon_id', 'bank_id', 'changed_by_user_id'], 'integer'],
+            [['insalon_id', 'bank_id', 'changed_by_user_id'], 'required'],
+            [['changed'], 'safe'],
+            [['state_id'], 'string', 'max' => 10],
+            [['state_desc', 'b1', 'b2', 'b3', 'b4', 'b5'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'Номер',
+            'active' => 'Активна',
+            'insalon_id' => 'Insalon ID',
+            'bank_id' => 'Bank ID',
+            'changed' => 'Изменена',
+            'changed_by_user_id' => 'Изменена пользователем',
+            'state_id' => 'State ID',
+            'state_desc' => 'State Desc',
+            'b1' => 'B1',
+            'b2' => 'B2',
+            'b3' => 'B3',
+            'b4' => 'B4',
+            'b5' => 'B5',
+        ];
+    }
+}
