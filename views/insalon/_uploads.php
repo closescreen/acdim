96566594ec17486dc73d87fe3ee328cb1ad84616
closescreen <close.screen@gmail.com>
2017-09-27 14:48:16 +0300

    обработка "файл не найден"

--- views/insalon/_uploads.php
+++ views/insalon/_uploads.php
@@ -20,12 +20,19 @@ $this->title = '';
 
             $class = $upload->created_by_user_id == Yii::$app->user->identity->id ?
                     "alert alert-success" : "alert alert-secondary";
+
+            $file_exists = is_file($upload->file_real_name);
         ?>
 
-        <div class="<?=$class?>">
-            <?= Html::a($upload->file_name, ['upload/download','id'=>$upload->id]) ?>
-            ( <?= Html::encode( $upload->file_desc ) ?> ) <br>
-        </div>
+    <div class="<?=$class?>">
+        <? if($file_exists): ?>
+             <?= Html::a($upload->file_name, ['upload/download','id'=>$upload->id]) ?>
+             ( <?= Html::encode( $upload->file_desc ) ?> )  <br>
+
+        <? else: ?>
+            <?= '(файл не найден) '. $upload->file_name .' ('. Html::encode( $upload->file_desc ).')' ?>
+        <? endif; ?>
+    </div>
 
      <? endforeach; ?>
 
