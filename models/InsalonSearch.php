0c074936f53288dcc1eaf3d6729f0246f13e5dc1
closescreen <close.screen@gmail.com>
2017-09-28 11:41:05 +0300

    insalon/index - почти все фильтры пренес в like

--- models/InsalonSearch.php
+++ models/InsalonSearch.php
@@ -22,15 +22,33 @@ class InsalonSearch extends InsalonMaxStateLastMsg //Insalon
     {
         return [
             [[
-                'id', 'active', 'salon_id', 'created_by_user_id',
-                'changed_by_user_id', 'car_price', 'down_payment',
-                'equipment_cost', 'car_year', 'state_stage',
-                'm_id', 'm_inbank_id', 'm_created_by_user_id'],
+                'id',
+                'active',
+                'salon_id',
+                'created_by_user_id',
+                'changed_by_user_id',
+                'car_price',
+                'down_payment',
+                'equipment_cost',
+                'car_year',
+                'state_stage',
+                'm_id',
+                'm_inbank_id',
+                'm_created_by_user_id'],
                     'integer'],
-            [['created', 'changed', 'client_fname', 'client_sname',
-                'client_tname', 'client_bdate', 'client_phone',
-                'equipment_desc', 'car_model',
-                's1', 's2', 's3', 's4', 's5','state_id','state_name',
+            [['created',
+                'changed',
+                'client_fname',
+                'client_sname',
+                'client_tname',
+                'client_fio',
+                'client_bdate',
+                'client_phone',
+                'equipment_desc',
+                'car_model',
+                's1', 's2', 's3', 's4', 's5',
+                'state_id',
+                'state_name',
                 'm_created_text',
                 'm_created',
                 'salon_name' ],
@@ -87,34 +105,44 @@ class InsalonSearch extends InsalonMaxStateLastMsg //Insalon
 
         // grid filtering conditions
         $query->andFilterWhere([
-            'id' => $this->id,
+          //  'id' => $this->id,
             'active' => $this->active,
             'salon_id' => $this->salon_id,
-            'created' => $this->created,
             'created_by_user_id' => $this->created_by_user_id,
-            'changed' => $this->changed,
             'changed_by_user_id' => $this->changed_by_user_id,
-            'client_bdate' => $this->client_bdate,
-            'car_price' => $this->car_price,
-            'down_payment' => $this->down_payment,
-            'equipment_cost' => $this->equipment_cost,
-            'car_year' => $this->car_year,
+            //'client_bdate' => $this->client_bdate,
+            //'car_price' => $this->car_price,
+            //'down_payment' => $this->down_payment,
+            //'equipment_cost' => $this->equipment_cost,
+//            'car_year' => $this->car_year,
 
         ]);
 
-        $query->andFilterWhere(['like', 'client_fname', $this->client_fname])
+        $query
             ->andFilterWhere(['like', 'salon_name', $this->salon_name])
+            ->andFilterWhere(['like', 'id', $this->id])
+            ->andFilterWhere(['like', 'client_fname', $this->client_fname])
             ->andFilterWhere(['like', 'client_sname', $this->client_sname])
             ->andFilterWhere(['like', 'client_tname', $this->client_tname])
+            ->andFilterWhere(['like', 'client_fio', $this->client_fio])
             ->andFilterWhere(['like', 'client_phone', $this->client_phone])
+            ->andFilterWhere(['like', 'client_bdate', $this->client_bdate])
             ->andFilterWhere(['like', 'equipment_desc', $this->equipment_desc])
             ->andFilterWhere(['like', 'car_model', $this->car_model])
+            ->andFilterWhere(['like', 'car_price', $this->car_price])
+            ->andFilterWhere(['like', 'down_payment', $this->down_payment])
+            ->andFilterWhere(['like', 'equipment_cost', $this->equipment_cost])
             ->andFilterWhere(['like','state_name', $this->state_name])
             ->andFilterWhere(['like', 's1', $this->s1])
             ->andFilterWhere(['like', 's2', $this->s2])
             ->andFilterWhere(['like', 's3', $this->s3])
             ->andFilterWhere(['like', 's4', $this->s4])
-            ->andFilterWhere(['like', 's5', $this->s5]);
+            ->andFilterWhere(['like', 's5', $this->s5])
+            ->andFilterWhere(['like', 'm_created_text', $this->m_created_text])
+            ->andFilterWhere(['like', 'created', $this->created])
+            ->andFilterWhere(['like', 'changed', $this->changed])
+            ->andFilterWhere(['like', 'car_year', $this->car_year])
+        ;
 
         return $dataProvider;
     }
